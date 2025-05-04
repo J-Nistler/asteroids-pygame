9003c9bf4dcf2f73caaf96ba3d46a4814ffe44f3
J-Nistler <jmnistl@protonmail.com>
2025-05-04 09:13:59 -0400

    add circleshape.py file

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def main ():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     running = True
+    dt = 0
 
     # infinite game loop
     while running:
@@ -22,6 +24,7 @@ def main ():
     screen.fill("black")
 
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
